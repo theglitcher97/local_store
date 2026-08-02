9f134a33f00d8c36fcbbeeaf10fea32c8444ad8a
fernando <forozcomercado@gmail.com>
2026-08-02 13:53:42 -0500

    feat: add initial api security configuration; make auth endpoints unrestricted

--- src/main/java/com/store/local_store/web/config/ApiSecurityConfig.java
+++ src/main/java/com/store/local_store/web/config/ApiSecurityConfig.java
@@ -1,4 +1,29 @@
 package com.store.local_store.web.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
 public class ApiSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+        httpSecurity.authorizeHttpRequests(http ->
+                http.
+                        requestMatchers("/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
 }
