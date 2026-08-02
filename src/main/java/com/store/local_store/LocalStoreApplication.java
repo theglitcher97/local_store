1aa8f6bb8ab5b4223692ea242ad894ab08e0bae5
fernando <forozcomercado@gmail.com>
2026-08-02 11:19:05 -0500

    Innit commit

---
+++ src/main/java/com/store/local_store/LocalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.store.local_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/local_store/LocalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.local_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
