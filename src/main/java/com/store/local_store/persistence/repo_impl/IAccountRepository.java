f812ccff0d19082bad85941733d6a8f9ae87bdaa
fernando <forozcomercado@gmail.com>
2026-08-02 21:27:38 -0500

    fix: encrypt password before saving new account

--- src/main/java/com/store/local_store/persistence/repo_impl/IAccountRepository.java
+++ src/main/java/com/store/local_store/persistence/repo_impl/IAccountRepository.java
@@ -4,16 +4,18 @@ import com.store.local_store.domain.ports.repos.AccountRepository;
 import com.store.local_store.persistence.entities.UserEntity;
 import com.store.local_store.persistence.repositories.UserEntityRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class IAccountRepository implements AccountRepository {
     private UserEntityRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void createAccount(String email, String password) {
-        UserEntity userEntity = new UserEntity(null, email, password, "ROLE_CUSTOMER");
+        UserEntity userEntity = new UserEntity(null, email, this.passwordEncoder.encode(password), "ROLE_CUSTOMER");
         this.userRepository.save(userEntity);
     }
 }
